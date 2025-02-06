d6a3eaa20e501f721d20eccdac75dc8680aa6967
Emre Dayangaç <120194760+EmreDay1@users.noreply.github.com>
2025-02-06 20:54:11 +0300

    Update test.js

--- src/test.js
+++ src/test.js
@@ -1,47 +1,22 @@
-// test.js
-import { Lexer, Parser } from './lexer_and_parser.mjs';
+import { LogicalLexer, LogicalParser } from './logical_lexer_and_parser.mjs';
 import { Interpreter } from './interpreter.mjs';
 
-const testCode = `
-param width 200
-param height 150
-param radius width * 0.2
-
-shape rectangle main {
-    width: param.width
-    height: param.height
-    position: [0 0]
-}
-
-shape circle center {
-    radius: param.radius
-    position: [param.width/2 param.height/2]
-}
+const input = `
+if circle.radius > 10 and circle.radius < 20:
+    rectangle box1
+        width: 100
+        height: 50
+else:
+    circle small_circle
+        radius: 5
 `;
 
-console.log("Testing AQUI Program\n");
-console.log("Input Program:");
-console.log(testCode);
+const lexer = new LogicalLexer(input);
+const parser = new LogicalParser(lexer);
+const ast = parser.parse();
 
-try {
-    // Parse
-    const lexer = new Lexer(testCode);
-    const parser = new Parser(lexer);
-    const ast = parser.parse();
-    
-    console.log("\nAST:");
-    console.log(JSON.stringify(ast, null, 2));
+const interpreter = new Interpreter();
+const result = interpreter.interpret(ast);
 
-    // Interpret
-    const interpreter = new Interpreter();
-    const result = interpreter.interpret(ast);
-    
-    console.log("\nFinal Environment:");
-    console.log("Parameters:", Object.fromEntries(result.parameters));
-    console.log("Shapes:", Object.fromEntries(result.shapes));
-    console.log("Layers:", Object.fromEntries(result.layers));
-
-} catch (error) {
-    console.error("\nError:", error.message);
-    console.error("\nStack trace:", error.stack);
-}
+console.log('\nResult:');
+console.log(result);
